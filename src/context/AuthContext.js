2c7b886717453a066ab6c74870b2b0ade4b5c16a
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-04 13:53:53 -0700

    saved user info to context with dispatch from useReducer when user logs in

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,6 +4,11 @@ export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
   switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        user: action.payload,
+      };
     default:
       return state;
   }
@@ -13,7 +18,7 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
-
+  console.log({ 'AuthContext state': state });
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { projectAuth } from '../firebase/config';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password, displayName) => {
     setError(null);
@@ -13,12 +15,14 @@ export const useSignup = () => {
         email,
         password
       );
-      console.log(response.user);
+
       if (!response) {
         throw new Error('Could not complete signup');
       }
       //   add display name to user
       await response.user.updateProfile({ displayName });
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: response.user });
     } catch (error) {
       console.error(error);
       setError(error.message);
